c51f9dfee91dbbfc504c9d2950a708681b80dac9
Clentan <105500834+Clentan@users.noreply.github.com>
2024-04-23 10:58:46 +0200

    java

---
+++ java.js
@@ -0,0 +1,20 @@
+
+var today = new Date(); //Creat a new date object
+var hourNow = today.getHours(); // find the current time
+var greeting;
+
+//dispaly greeting suitable for the current time
+if (hourNow > 18)
+{greeting = 'Good Evening';}
+else if (hourNow > 12)
+{
+    greeting = 'Good afternoon';
+}
+else if (hourNow >o)
+{
+    greeting = 'Good Morning'
+}
+else {
+    greeting='Welcome'
+}
+document.write(greeting);
