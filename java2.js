7b2827550de1ab3b332839ce3508e10b20ef4bfa
Clentan <105500834+Clentan@users.noreply.github.com>
2024-04-23 14:47:44 +0200

    formspree addition

--- java2.js
+++ java2.js
@@ -24,8 +24,8 @@ function validate(){
       error_message.innerHTML = text;
       return false;
     }
-    if(message.length <= 140){
-      text = "Please Enter More Than 140 Characters";
+    if(message.length <= 40){
+      text = "Please Enter More Than 40 Characters";
       error_message.innerHTML = text;
       return false;
     }
